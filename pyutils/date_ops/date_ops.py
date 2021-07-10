cab8fcffbe52863345d07eafbe1eb7b52db8c6e3
Nishant173 <nishant.rao173@gmail.com>
2021-07-10 11:07:59 +0530

    Updated date_ops

--- pyutils/date_ops/date_ops.py
+++ pyutils/date_ops/date_ops.py
@@ -1,20 +1,23 @@
 import datetime
+from datetime import timedelta
 
 import pandas as pd
 from randomtimestamp import randomtimestamp
 
+TIMESTAMP_FORMAT = "%Y%m%d%H%M%S" # Will be converted to an integer after parsing
+
 
 def get_current_timestamp() -> int:
     dt_obj = datetime.datetime.now()
-    ts_now = dt_obj.strftime("%Y%m%d%H%M%S")
+    ts_now = dt_obj.strftime(TIMESTAMP_FORMAT)
     return int(ts_now)
 
 
 def get_random_timestamp() -> int:
     ts_random = randomtimestamp(
-        start_year=2000,
-        end_year=2020,
-        pattern="%Y%m%d%H%M%S",
+        start_year=1900,
+        end_year=datetime.datetime.now().year - 1,
+        pattern=TIMESTAMP_FORMAT,
     )
     return int(ts_random)
 
@@ -55,3 +58,37 @@ def convert_to_timestamp(
         second=second,
     )
     return dt_obj
+
+
+def parse_date_string(date_string: str) -> datetime.datetime:
+    """
+    Parses date string of format 'yyyy-mm-dd' into datetime object.
+    >>> parse_date_string(date_string="2020-03-28")
+    """
+    dt_obj = datetime.datetime.strptime(date_string, "%Y-%m-%d")
+    return dt_obj
+
+
+def parse_datetime_string(datetime_string: str) -> datetime.datetime:
+    """
+    Parses datetime string of format 'yyyy-mm-dd hh:mm:ss' into datetime object.
+    Note: The 'hour' in the `datetime_string` must be of 24 hour format (from 0-23).
+    >>> parse_datetime_string(datetime_string="2020-03-28 17:53:04")
+    """
+    dt_obj = datetime.datetime.strptime(datetime_string, "%Y-%m-%d %H:%M:%S")
+    return dt_obj
+
+
+def utc_to_ist(dt_obj: datetime.datetime) -> datetime.datetime:
+    """Adds 5 hours and 30 minutes"""
+    return dt_obj + timedelta(hours=5, minutes=30)
+
+
+def ist_to_utc(dt_obj: datetime.datetime) -> datetime.datetime:
+    """Subtracts 5 hours and 30 minutes"""
+    return dt_obj - timedelta(hours=5, minutes=30)
+
+
+def convert_to_naive_timezone(dt_obj: datetime.datetime) -> datetime.datetime:
+    dt_obj_converted = dt_obj.replace(tzinfo=None)
+    return dt_obj_converted
