6dc381931314d2f7ee1eb2f4660ae65bd86545d4
Nishant173 <nishant.rao173@gmail.com>
2021-08-10 10:58:55 +0530

    Added func 'style_dataframe_by_cell()'

--- pyutils/excel_ops/excel_ops.py
+++ pyutils/excel_ops/excel_ops.py
@@ -16,6 +16,79 @@ from pyutils.general.data_io import get_basename_from_filepath
 Column = Union[int, float, str] # Type-hint for columns in a Pandas DataFrame
 
 
+def style_dataframe_by_cell(
+        df_style_obj: Any,
+        props: List[Dict[str, Union[pd.Series, List[Column], Dict[str, str], None]]],
+    ) -> Any:
+    """
+    Takes in Pandas Styler object (of class `pandas.io.formats.style.Styler`), and a list of `props`.
+    Returns Pandas Styler object after applying the styles based on the given conditions (via `props`).
+    Reference: https://pandas.pydata.org/pandas-docs/stable/user_guide/style.html
+
+    >>> num_rows = 50
+    >>> df = pd.DataFrame(data={
+        'column1': [np.random.randint(-25, 25) for _ in range(num_rows)],
+        'column2': [np.random.randint(0, 100) for _ in range(num_rows)],
+        'column3': [np.random.randint(-40, 15) for _ in range(num_rows)],
+        'column4': [np.random.randint(-5, 5) for _ in range(num_rows)],
+    })
+    >>> style_dataframe_by_cell(
+        df_style_obj=df.style,
+        props=[
+            {
+                'row_conditions': (df['column1'] >= 10) | (df['column1'] % 2 == 0),
+                'column_subset': ['column1', 'column2'],
+                'styling_kwargs': {
+                    'background-color': 'green',
+                    'color': 'purple',
+                },
+            },
+            {
+                'row_conditions': (df['column3'] < 0),
+                'column_subset': None,
+                'styling_kwargs': {
+                    'background-color': 'blue',
+                    'color': 'orange',
+                },
+            },
+            {
+                'row_conditions': None,
+                'column_subset': ['column4'],
+                'styling_kwargs': {
+                    'background-color': 'grey',
+                    'color': 'red',
+                },
+            },
+        ],
+    )
+
+    In the `props`, each prop is a dictionary having 3 keys: ['row_conditions', 'column_subset', 'styling_kwargs'].
+    The keys 'row_conditions' and 'column_subset' can have a value of None.
+    The key 'styling_kwargs' can have a value of {}.
+    The ordering of the `props` matter i.e; if 5 props are given, the 4th prop can over-write the 3rd prop.
+    """
+    df_styled = df_style_obj.__copy__()
+    idx = pd.IndexSlice
+    for prop in props:
+        row_conditions = prop['row_conditions']
+        column_subset = prop['column_subset']
+        styling_kwargs = prop['styling_kwargs']
+
+        subset = idx[idx[row_conditions], column_subset]
+        if row_conditions is None and column_subset is None:
+            subset = None
+        elif row_conditions is None and column_subset is not None:
+            subset = idx[idx[:], column_subset]
+        elif row_conditions is not None and column_subset is None:
+            subset = idx[idx[row_conditions], :]
+        df_styled = df_styled.set_properties(
+            subset=subset,
+            axis=1,
+            **styling_kwargs,
+        )
+    return df_styled
+
+
 def style_dataframe(
         data: pd.DataFrame,
         blue_white_red: Optional[List[Column]] = None,
