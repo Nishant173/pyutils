d4d3034a075f708ed1e8c881baea2074bb2c33c7
Nishant173 <nishant.rao173@gmail.com>
2021-08-13 17:05:09 +0530

    Refactored email_ops

--- pyutils/email_ops/email_ops.py
+++ pyutils/email_ops/email_ops.py
@@ -11,6 +11,41 @@ from pyutils.general.data_io import (
 )
 
 
+def add_attachments_to_multipart_obj(
+        multipart_obj: MIMEMultipart,
+        filepaths_to_attachments: List[str],
+    ) -> MIMEMultipart:
+    """
+    Adds attachments from the given filepaths to the given `MIMEMultipart` object.
+    Returns `MIMEMultipart` object containing the attachments.
+    """
+    extension_to_mime_subtype_mapper = {
+        'csv': 'octet-stream',
+        'docx': 'octet-stream',
+        'pdf': 'pdf',
+        'xls': 'octet-stream',
+        'xlsx': 'octet-stream',
+    }
+    for filepath in filepaths_to_attachments:
+        extension = get_extension(filepath=filepath)
+        mime_subtype = extension_to_mime_subtype_mapper.get(extension.lower(), None)
+        if mime_subtype is None:
+            raise Exception(
+                f"Cannot attach '{filepath}', as this file extension is not supported.",
+                f" Supported extensions are: {list(extension_to_mime_subtype_mapper.keys())}"
+            )
+        attachment = open(file=filepath, mode='rb')
+        payload = MIMEBase(_maintype='application', _subtype=mime_subtype)
+        payload.set_payload(payload=attachment.read())
+        encoders.encode_base64(msg=payload)
+        payload.add_header(
+            _name='Content-Disposition',
+            _value=f"attachment; filename={get_basename_from_filepath(filepath=filepath)}",
+        )
+        multipart_obj.attach(payload=payload)
+    return multipart_obj
+
+
 def send_email(
         from_email_id: str,
         from_email_id_password: str,
@@ -47,34 +82,11 @@ def send_email(
     msg['Cc'] = ", ".join(cc_email_ids)
     msg['Subject'] = subject
     msg.attach(payload=MIMEText(_text=body, _subtype='html'))
-    
     if filepaths_to_attachments is not None:
-        extension_to_mime_subtype_mapper = {
-            'csv': 'octet-stream',
-            'docx': 'octet-stream',
-            'pdf': 'pdf',
-            'xls': 'octet-stream',
-            'xlsx': 'octet-stream',
-        }
-        
-        for filepath in filepaths_to_attachments:
-            extension = get_extension(filepath=filepath)
-            mime_subtype = extension_to_mime_subtype_mapper.get(extension.lower(), None)
-            if mime_subtype is None:
-                raise Exception(
-                    f"Cannot attach '{filepath}', as this file extension is not supported.",
-                    f" Supported extensions are: {list(extension_to_mime_subtype_mapper.keys())}"
-                )
-            attachment = open(file=filepath, mode='rb')
-            payload = MIMEBase(_maintype='application', _subtype=mime_subtype)
-            payload.set_payload(payload=attachment.read())
-            encoders.encode_base64(msg=payload)
-            payload.add_header(
-                _name='Content-Disposition',
-                _value=f"attachment; filename={get_basename_from_filepath(filepath=filepath)}",
-            )
-            msg.attach(payload=payload)
-    
+        msg = add_attachments_to_multipart_obj(
+            multipart_obj=msg,
+            filepaths_to_attachments=filepaths_to_attachments,
+        )
     server = smtplib.SMTP(host='smtp.gmail.com', port=587)
     server.starttls()
     server.ehlo()
