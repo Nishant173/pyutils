05ef92d8d31792b83ea6fbfcde24cbf8495e9092
Nishant173 <nishant.rao173@gmail.com>
2021-08-13 16:36:55 +0530

    Updating email attachment function (v2)

--- pyutils/email_ops/email_ops.py
+++ pyutils/email_ops/email_ops.py
@@ -1,38 +1,43 @@
-from typing import List
+from typing import List, Optional
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
 
-from pyutils.general.data_io import get_basename_from_filepath
+from pyutils.general.data_io import (
+    get_basename_from_filepath,
+    get_extension,
+)
 
 
-def send_email_with_excel_attachments(
+def send_email(
         from_email_id: str,
         from_email_id_password: str,
         to_email_ids: List[str],
         cc_email_ids: List[str],
         subject: str,
         body: str,
-        excel_filepaths_with_ext: List[str],
+        filepaths_to_attachments: Optional[List[str]] = None,
     ) -> None:
     r"""
-    Sends an email from one Email ID to one or more Email IDs, along with the Excel attachments provided (if any).
+    Sends an email from one Email ID to one or more Email IDs, along with the attachments provided (if any).
     Accepts HTML tags for the `body` parameter.
     Note: Works only if the sender uses a Gmail ID.
 
-    >>> send_email_with_excel_attachments(
+    >>> send_email(
             from_email_id='sender_email_id@gmail.com',
             from_email_id_password='some_password',
             to_email_ids=['person1@gmail.com', 'person2@gmail.com'],
             cc_email_ids=['person3@gmail.com'],
             subject="Your subject",
             body="Your message",
-            excel_filepaths_with_ext=[
-                r"C:\user\files\file1.xlsx",
-                r"C:\user\files\file2.xlsx",
-                r"C:\user\files\file3.xlsx",
+            filepaths_to_attachments=[
+                r"C:\user\files\file1.csv",
+                r"C:\user\files\file2.docx",
+                r"C:\user\files\file3.pdf",
+                r"C:\user\files\file4.xls",
+                r"C:\user\files\file5.xlsx",
             ],
         )
     """
@@ -42,6 +47,32 @@ def send_email_with_excel_attachments(
     msg['Cc'] = ", ".join(cc_email_ids)
     msg['Subject'] = subject
     msg.attach(payload=MIMEText(_text=body, _subtype='html'))
+    
+    extension_to_mime_subtype_mapper = {
+        'csv': 'octet-stream',
+        'docx': 'octet-stream',
+        'pdf': 'pdf',
+        'xls': 'octet-stream',
+        'xlsx': 'octet-stream',
+    }
+
+    for filepath in filepaths_to_attachments:
+        extension = get_extension(filepath=filepath)
+        mime_subtype = extension_to_mime_subtype_mapper.get(extension.lower(), None)
+        if mime_subtype is None:
+            raise Exception(
+                f"Cannot attach '{filepath}', as this file extension is not supported.",
+                f" Supported extensions are: {list(extension_to_mime_subtype_mapper.keys())}"
+            )
+        attachment = open(file=filepath, mode='rb')
+        payload = MIMEBase(_maintype='application', _subtype=mime_subtype)
+        payload.set_payload(payload=attachment.read())
+        encoders.encode_base64(msg=payload)
+        payload.add_header(
+            _name='Content-Disposition',
+            _value=f"attachment; filename={get_basename_from_filepath(filepath=filepath)}",
+        )
+        msg.attach(payload=payload)
     
     server = smtplib.SMTP(host='smtp.gmail.com', port=587)
     server.starttls()
