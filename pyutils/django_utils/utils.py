1e7dc9d70cbe126ffbbc65901efb4cf6437bdd71
Nishant173 <nishant.rao173@gmail.com>
2021-08-14 21:44:11 +0530

    Added func 'file_to_django_http_response()'

--- pyutils/django_utils/utils.py
+++ pyutils/django_utils/utils.py
@@ -1,12 +1,15 @@
 from typing import Dict, List, Optional, Union
+import mimetypes
 
 from django.db.models import QuerySet
+from django.http import HttpResponse
 import pandas as pd
 
 from pyutils.data_analysis.transform import (
     dataframe_to_list,
     drop_columns_if_exists,
 )
+from pyutils.general.data_io import get_basename_from_filepath
 
 
 def queryset_to_dataframe(
@@ -26,3 +29,19 @@ def queryset_to_list(
     df = queryset_to_dataframe(qs=qs, fields_to_drop=fields_to_drop)
     list_of_dicts = dataframe_to_list(data=df)
     return list_of_dicts
+
+
+def file_to_django_http_response(filepath: str) -> HttpResponse:
+    """
+    Converts the given file to Django HTTP Response object.
+    Works for the following file extensions: ['csv', 'docx', 'flv', 'jpg', 'm4a', 'mp3', 'mp4', 'pdf', 'png', 'txt', 'xls', 'xlsx', 'zip'].
+    """
+    type_, encoding = mimetypes.guess_type(url=filepath)
+    if type_ is None or encoding is not None:
+        type_ = 'application/octet-stream'
+    with open(file=filepath, mode='rb') as fp:
+        response = HttpResponse(content=fp, content_type=type_)
+    filename = get_basename_from_filepath(filepath=filepath)
+    response['Content-Disposition'] = f'attachment; filename="{filename}"'
+    response['X-Sendfile'] = filename
+    return response
