a95d7fe0e3e0421e69f9ecfe4f33ae0c595f7846
Nishant173 <nishant.rao173@gmail.com>
2021-11-06 22:12:11 +0530

    django_ops - Added decorator and utils

--- pyutils/frameworks/django_ops/decorators.py
+++ pyutils/frameworks/django_ops/decorators.py
@@ -1,10 +1,15 @@
-from typing import Callable
+from typing import Callable, Optional
 import functools
+import traceback
 
 from django.db import (
     connection,
     reset_queries,
 )
+from rest_framework import status
+from rest_framework.response import Response
+
+from pyutils.frameworks.django_ops.utils import get_django_request_info_from_args
 
 
 def django_query_counter(func: Callable) -> Callable:
@@ -21,3 +26,35 @@ def django_query_counter(func: Callable) -> Callable:
         )
         return result
     return wrapper_django_query_counter
+
+
+def api_endpoint_exception_handler(
+        api_endpoint_description: str,
+        print_response: Optional[bool] = False,
+    ) -> Callable:
+    """
+    Decorator that handles exceptions that may occur in function based API endpoints.
+    Expects a function that takes in a `rest_framework.request.Request` object, and
+    returns a `rest_framework.response.Response` object.
+    If an exception is encountered, then a response with status code 500 is returned.
+    """
+    def decorator(func: Callable) -> Callable:
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs) -> Response:
+            try:
+                response_obj = func(*args, **kwargs)
+                return response_obj
+            except Exception as exc:
+                response = {
+                    'exception_type': type(exc).__name__,
+                    'exception_msg': ' | '.join(exc.args),
+                    'api_endpoint_function_name': func.__name__,
+                    'api_endpoint_description': api_endpoint_description,
+                    'traceback_string': traceback.format_exc(),
+                    'request_info': get_django_request_info_from_args(*args),
+                }
+                if print_response:
+                    print(f"An exception occurred. Response: {response}")
+                return Response(data=response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return wrapper
+    return decorator
