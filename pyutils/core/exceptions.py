c72340d48caa28be4e6fcd75b83b22c77dc2472d
Nishant173 <nishant.rao173@gmail.com>
2021-11-06 22:12:32 +0530

    Added another custom Exception class

--- pyutils/core/exceptions.py
+++ pyutils/core/exceptions.py
@@ -1,3 +1,8 @@
 class InvalidDataFrameError(Exception):
     """Error raised when an Invalid DataFrame is encountered (based on certain expectations)"""
     pass
+
+
+class MissingRequiredParameterError(Exception):
+    """Error raised when a required parameter is missing"""
+    pass
