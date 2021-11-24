313a8c133a99d490960fd5d2c41e42cb8369fc2a
Nishant173 <nishant.rao173@gmail.com>
2021-11-24 21:04:52 +0530

    Added exception handling utility function

--- pyutils/core/exceptions.py
+++ pyutils/core/exceptions.py
@@ -1,4 +1,4 @@
-from typing import Any, List
+from typing import Any, List, Type
 
 
 def raise_exception_if_invalid_option(
@@ -9,6 +9,17 @@ def raise_exception_if_invalid_option(
     """Raises a ValueError if the `option_value` given is an invalid option; otherwise returns None"""
     if option_value not in valid_option_values:
         raise ValueError(f"Expected `{option_name}` to be in {valid_option_values}, but got {option_value}")
+    return None
+
+
+def raise_exception_if_invalid_type(
+        parameter_name: str,
+        parameter_value: Any,
+        expected_type: Type,
+    ) -> None:
+    """Raises a TypeError if the `parameter_value` given is not of the type `expected_type`; otherwise returns None"""
+    if not isinstance(parameter_value, expected_type):
+        raise TypeError(f"Expected `{parameter_name}` to be of type `{expected_type}`, but got type `{type(parameter_value)}`")
     return None
 
 
