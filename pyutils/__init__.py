1778c55e92ac135ea3c22634b2f88a18353ec882
Nishant173 <nishant.rao173@gmail.com>
2022-02-21 16:55:02 +0530

    Updated pyutils/__init__.py

--- pyutils/__init__.py
+++ pyutils/__init__.py
@@ -4,3 +4,9 @@ from pyutils import (
     data_wrangler,
     frameworks,
 )
+from setup import PACKAGE_VERSION
+
+
+__author__ = "Nishant Rao"
+__email__ = "nishant.rao173@gmail.com"
+__version__ = PACKAGE_VERSION
